658b5577cdc19ace658eb84f2d78bfb0a36f2f80
C. Andy Martin <charles_martin@jabil.com>
2023-05-12 16:15:12 -0400

    handle SIGTERM/SIGINT in node
    If the node receives SIGTERM/SIGINT shutdown cleanly.

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,19 @@
+#include <signal.h>
+
 #include "ros/ros.h"
 #include "orbbec_camera/ob_camera_node_driver.h"
 
+void term_handler(int) {
+  ros::requestShutdown();
+}
+
 int main(int argc, char** argv) {
+  /*
+   * handle SIGTERM and SIGINT so that we can shutdown cleanly. We have seen
+   * cameras get into bad states when this is not shutdown cleanly.
+   */
+  signal(SIGTERM, term_handler);
+  signal(SIGINT, term_handler);
   ros::init(argc, argv, "orbbec_camera");
   ros::NodeHandle nh;
   ros::NodeHandle nh_private("~");
