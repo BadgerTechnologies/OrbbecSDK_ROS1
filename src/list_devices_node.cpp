cd88dc6ee31d3d8190cdfa8519553fb82d254a90
C. Andy Martin <charles_martin@jabil.com>
2023-05-12 15:07:23 -0400

    badger: list_devices_node: add legacy format
    Badger specific format, parsed by depthcam ID scripts, based on
    legacy ros_astra_camera format. The parsing scripts need the
    formatted text to not have ROS log headers, so output directly to
    stdout.

--- src/list_devices_node.cpp
+++ src/list_devices_node.cpp
@@ -8,6 +8,17 @@ int main() {
   for (size_t i = 0; i < list->deviceCount(); i++) {
     auto serial = list->getDevice(i)->getDeviceInfo()->serialNumber();
     ROS_INFO_STREAM("serial: " << serial);
+    // Badger specific format, parsed by depthcam ID scripts, based on legacy
+    // ros_astra_camera format. The parsing scripts need the formatted text to
+    // not have ROS log headers, so output directly to stdout.
+    auto device_info = list->getDevice(i)->getDeviceInfo();
+    std::cout << "Device #" << i << ":" << std::endl;
+    std::cout << "Name: " << device_info->name() << std::endl;
+    std::cout << "Firmware version: " << device_info->firmwareVersion() << std::endl;
+    std::cout << "UID: " << device_info->uid() <<
+      " (Vendor ID: " << std::hex << device_info->vid() <<
+      ", Product ID: " << std::hex << device_info->pid() << ")" << std::endl;
+    std::cout << "Serial number: " << serial << std::endl;
   }
   return 0;
 }
