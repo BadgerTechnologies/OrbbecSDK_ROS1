971d6eb1b725f655d6d0a119604a13913ebbaed7
C. Andy Martin <charles_martin@jabil.com>
2023-05-12 13:57:42 -0400

    badger: provide dyncfg for disabling emitter
    Provide a legacy (backwards-compatibile) interface via dynamic
    reconfigure to disable the IR emitter (laser). It would take a bit of
    work to hunt down the various places that either disable the emitter
    or need to know its state and figure out how to rearchitect them. In
    at least one place, the code is relying on the dynamic reconfigure
    parameter update topic to get changes to the emitter state. One way
    to rearchitect would be to use resource manager to send state changes
    and have it manage tracking the state of the emitter.

--- include/orbbec_camera/ob_camera_node.h
+++ include/orbbec_camera/ob_camera_node.h
@@ -5,6 +5,7 @@
 #include "ros/ros.h"
 #include <opencv2/opencv.hpp>
 #include <cv_bridge/cv_bridge.h>
+#include <dynamic_reconfigure/server.h>
 #include <sensor_msgs/CameraInfo.h>
 #include <sensor_msgs/PointCloud2.h>
 #include <sensor_msgs/distortion_models.h>
@@ -19,9 +20,13 @@
 #include <std_srvs/Empty.h>
 #include "orbbec_camera/d2c_viewer.h"
 #include "orbbec_camera/GetCameraParams.h"
+#include "orbbec_camera/OrbbecCameraConfig.h"
 #include <boost/optional.hpp>
 
 namespace orbbec_camera {
+
+using ReconfigureServer = dynamic_reconfigure::Server<OrbbecCameraConfig>;
+
 class OBCameraNode {
  public:
   OBCameraNode(ros::NodeHandle& nh, ros::NodeHandle& nh_private,
@@ -111,6 +116,8 @@ class OBCameraNode {
   int getCameraParamIndex();
 
   void setupCameraInfo();
+
+  void reconfigureCallback(OrbbecCameraConfig& config);
 
   // camera control services
   bool setMirrorCallback(std_srvs::SetBoolRequest& request, std_srvs::SetBoolResponse& response,
@@ -232,6 +239,7 @@ class OBCameraNode {
   std::string camera_link_frame_id_ = "camera_link";
   std::string camera_name_ = "camera";
   std::string base_frame_id_ = "camera_link";
+  std::unique_ptr<ReconfigureServer> reconfigure_server_ = nullptr;
   std::map<stream_index_pair, ros::ServiceServer> get_exposure_srv_;
   std::map<stream_index_pair, ros::ServiceServer> set_exposure_srv_;
   std::map<stream_index_pair, ros::ServiceServer> reset_exposure_srv_;
